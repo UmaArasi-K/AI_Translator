643d33dbec439f7be9d658c119f940fad4a82c4c
UmaArasi-K <umaarasi.k@gmail.com>
2024-04-05 12:59:55 +0530

    .

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from starlette.responses import FileResponse
+
 
 app = FastAPI()
 
 @app.get("/")
 async def read_root():
-    return {"message": "Hello, World"}
+    return FileResponse("template/html/index.html")
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
